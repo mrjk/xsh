8acb74477c5d55adfa48341a3037488e20844d26
mrjk <mrjk.78@gmail.com>
2024-04-19 20:25:49 -0400

    fix: ellipsis install dir

--- ellipsis.sh
+++ ellipsis.sh
@@ -3,14 +3,14 @@
 
 pkg.link() {
     # Link xsh in the bin folder
-    fs.link_file "$PKG_PATH/xsh" "$ELLIPSIS_HOME/.local/bin/xsh"
+    fs.link_file "$PKG_PATH/xsh.sh" "$ELLIPSIS_HOME/.local/bin/xsh.sh"
 }
 
 ##############################################################################
 
 pkg.unlink() {
     # Remove xsh link from the bin folder
-    rm "$ELLIPSIS_HOME/xsh"
+    rm "$ELLIPSIS_HOME/xsh.sh"
 }
 
 ##############################################################################
